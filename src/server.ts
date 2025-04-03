cdfbcd1d9a1701a561f3a6d7cce3721c113a20d5
Avinoam <eranis54321@gmail.com>
2025-04-03 12:01:18 +0300

    Ensure PORT is parsed as number for Render"

--- src/server.ts
+++ src/server.ts
@@ -43,14 +43,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('public', 'index.html'))
 })
 
-// START SERVER
 const PORT =
   process.env.NODE_ENV === 'development'
     ? 3000
-    : process.env.PORT
+    : Number(process.env.PORT)
 
 if (!PORT) {
-  throw new Error('❌ Missing PORT environment variable')
+  throw new Error('❌ Missing or invalid PORT environment variable')
 }
 
 async function startServer() {
